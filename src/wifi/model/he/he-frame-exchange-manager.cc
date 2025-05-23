nsnam/ns-3-dev-git

wifi: Extend RU allocation in MU-RTS for CTS frames transmitted over 320 MHz

src/wifi/model/he/he-frame-exchange-manager.cc
@@ -1265,10 +1265,14 @@ HeFrameExchangeManager::GetCtsTxVectorAfterMuRts(const CtrlTriggerHeader& trigge
     {
         bw = MHz_t{80};
     }
+    else if (ru == 68)
+    {
+        bw = MHz_t{160};
+    }
     else
     {
-        NS_ASSERT(ru == 68);
-        bw = MHz_t{160};
+        NS_ASSERT(ru == 69);
+        bw = MHz_t{320};
     }
 
     auto txVector = GetWifiRemoteStationManager()->GetCtsTxVector(m_bssid, GetCtsModeAfterMuRts());

src/wifi/model/wifi-protection-manager.cc
@@ -94,11 +94,11 @@ WifiProtectionManager::AddUserInfoToMuRts(CtrlTriggerHeader& muRts,
     auto phy = m_mac->GetWifiPhy(m_linkId);
     std::size_t primaryIdx = phy->GetOperatingChannel().GetPrimaryChannelIndex(ctsTxWidth);
     std::size_t idx80MHz = MHz_t{80} / ctsTxWidth;
-    if ((phy->GetChannelWidth() == MHz_t{160}) && (ctsTxWidth <= MHz_t{40}) &&
+    if ((phy->GetChannelWidth() > MHz_t{80}) && (ctsTxWidth <= MHz_t{40}) &&
         (primaryIdx >= idx80MHz))
     {
-        // the primary80 is in the higher part of the 160 MHz channel
-        primaryIdx -= idx80MHz;
+        // get the index of the primary within the primary80
+        primaryIdx %= idx80MHz;
     }
     switch (static_cast<uint16_t>(ctsTxWidth.in_MHz()))
     {
@@ -113,6 +113,9 @@ WifiProtectionManager::AddUserInfoToMuRts(CtrlTriggerHeader& muRts,
         break;
     case 160:
         ui.SetMuRtsRuAllocation(68);
+        break;
+    case 320:
+        ui.SetMuRtsRuAllocation(69);
         break;
     default:
         NS_ABORT_MSG("Unhandled TX width: " << ctsTxWidth);

DaniloLDCarvalho/TCP_Prague

src/wifi/model/he/he-frame-exchange-manager.cc
@@ -1241,10 +1241,14 @@ HeFrameExchangeManager::GetCtsTxVectorAfterMuRts(const CtrlTriggerHeader& trigge
     {
         bw = MHz_u{80};
     }
+    else if (ru == 68)
+    {
+        bw = MHz_u{160};
+    }
     else
     {
-        NS_ASSERT(ru == 68);
-        bw = MHz_u{160};
+        NS_ASSERT(ru == 69);
+        bw = MHz_u{320};
     }
 
     auto txVector = GetWifiRemoteStationManager()->GetCtsTxVector(m_bssid, GetCtsModeAfterMuRts());

src/wifi/model/wifi-protection-manager.cc
@@ -85,11 +85,11 @@ WifiProtectionManager::AddUserInfoToMuRts(CtrlTriggerHeader& muRts,
     auto phy = m_mac->GetWifiPhy(m_linkId);
     std::size_t primaryIdx = phy->GetOperatingChannel().GetPrimaryChannelIndex(ctsTxWidth);
     std::size_t idx80MHz = MHz_u{80} / ctsTxWidth;
-    if ((phy->GetChannelWidth() == MHz_u{160}) && (ctsTxWidth <= MHz_u{40}) &&
+    if ((phy->GetChannelWidth() > MHz_u{80}) && (ctsTxWidth <= MHz_u{40}) &&
         (primaryIdx >= idx80MHz))
     {
-        // the primary80 is in the higher part of the 160 MHz channel
-        primaryIdx -= idx80MHz;
+        // get the index of the primary within the primary80
+        primaryIdx %= idx80MHz;
     }
     switch (static_cast<uint16_t>(ctsTxWidth))
     {
@@ -104,6 +104,9 @@ WifiProtectionManager::AddUserInfoToMuRts(CtrlTriggerHeader& muRts,
         break;
     case 160:
         ui.SetMuRtsRuAllocation(68);
+        break;
+    case 320:
+        ui.SetMuRtsRuAllocation(69);
         break;
     default:
         NS_ABORT_MSG("Unhandled TX width: " << ctsTxWidth << " MHz");